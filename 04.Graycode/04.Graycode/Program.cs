bcfaf629cb5f17dec4b9194ec36d77ed1c3ba09a
Yathinkulal <87934584+Yathinkulal@users.noreply.github.com>
2022-02-17 12:52:07 +0530

    Add files via upload

---
+++ 04.Graycode/04.Graycode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercises
+
+{
+    class Graycode
+    {
+        static int getGray(int n)
+        {
+            return n ^ (n >> 1);
+        }
+        static void Main(string[] args)
+        {
+
+            int InputNum, GrayNum;
+            Console.WriteLine("\n Enter the decimal number");
+            InputNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n Binary equivalant of {0}:{1}", InputNum, Convert.ToString(InputNum, 2));
+
+
+            Console.WriteLine("\n Gray code equivalant of {0}:{1}", InputNum, Convert.ToString(InputNum, 2));
+
+
+        }
+    }
+}
