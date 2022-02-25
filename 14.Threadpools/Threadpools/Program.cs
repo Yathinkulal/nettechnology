15b92fb25c3f00699f39d8aa91abe222cf7eef72
Yathinkulal <87934584+Yathinkulal@users.noreply.github.com>
2022-02-25 12:27:48 +0530

    Add files via upload

---
+++ 14.Threadpools/Threadpools/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Exercises
+{
+    class ThreadPoolprog
+    {
+        public void ThreadFun1(object obj)
+        {
+            int loop = 0;
+            for(loop=0;loop<=4;loop++)
+            {
+                Console.WriteLine("Thread1 is executing");
+            }
+        }
+        public void ThreadFun2(object obj)
+        {
+            int loop = 0;
+            for(loop = 0;loop<=4;loop++)
+            {
+                Console.WriteLine("Thread2 is executing");
+            }
+        }
+        public static void Main()
+        {
+            ThreadPoolprog TP = new ThreadPoolprog();
+            for(int i=0;i<2;i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(TP.ThreadFun1));
+                ThreadPool.QueueUserWorkItem(new WaitCallback(TP.ThreadFun2));
+            }
+            Console.ReadKey();
+        }
+    }
+}
