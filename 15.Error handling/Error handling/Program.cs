736483ad1b56d334ec5e89456c816fd3355082f0
Yathinkulal <87934584+Yathinkulal@users.noreply.github.com>
2022-02-25 12:45:16 +0530

    Add files via upload

---
+++ 15.Error handling/Error handling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Error_handling
+{
+    class ExcepyionHandling
+    {
+        static void Main(string[] args)
+        {
+            Age a = new Age();
+            try
+            {
+                a.displayAge();
+            }
+            catch(AgelsNegtiveException e)
+            {
+                Console.WriteLine("AgelsNegativeException:{0}", e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Exception of finally block is done.");
+            }
+        }
+    }
+}
+public class AgelsNegtiveException:Exception
+{
+    public AgelsNegtiveException(string message):base(message)
+    {
+
+    }
+}
+public class Age
+{
+    int age = -5;
+    public void displayAge()
+    {
+        if(age<0)
+        {
+            throw (new AgelsNegtiveException("Age cannot be negative"));
+
+        }
+        else
+        {
+            Console.WriteLine("Age is:{0}",age);
+           
+        }
+    }
+}
