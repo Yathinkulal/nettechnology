13f3a2e21864e1dce6ea4efbfc108fb377aff3b2
Yathinkulal <87934584+Yathinkulal@users.noreply.github.com>
2022-03-18 10:47:10 +0530

    Add files via upload

---
+++ 18.PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+   public class PalindromeNumber
+    {
+        public static void Main(string[] args)
+        {
+        int n, r, sum = 0, temp;
+            Console.WriteLine("Enter the Number:");
+        n = int.Parse(Console.ReadLine());
+        temp = n;
+        while(n>0)
+        {
+            r = n % 10;
+            sum = (sum * 10) + r;
+            n = n / 10;
+
+        }
+        if(temp==sum)
+        {
+            Console.Write( sum+  "  Number is Palindrome");
+        }
+        else
+        {
+            Console.Write(sum+ "  Number is not Palindrome");
+        }
+        }
+    }
+
