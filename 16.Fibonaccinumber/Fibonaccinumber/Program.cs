76b8752c372e63967ab0f9e8c60511935a6c9d5a
Yathinkulal <87934584+Yathinkulal@users.noreply.github.com>
2022-03-18 10:45:06 +0530

    Add files via upload

---
+++ 16.Fibonaccinumber/Fibonaccinumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercises
+{
+    class FibonacciNumber
+    {
+        public static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, num;
+            Console.WriteLine("Enetr the number of element :");
+            num = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+            for (i = 2; i < num; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
